ad2e57d5b4924f717afff0b147295c928e28e15e
Bowie <marketing@letscode.com.br>
2025-04-29 17:41:06 -0300

    at2: lista,layout e outros

---
+++ app/src/main/java/com/devspace/recyclerview/ContactListAdapter.kt
@@ -0,0 +1,51 @@
+package com.devspace.recyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactListAdapter:
+    ListAdapter<Contact, ContactListAdapter.ContactViewHolder>(ContactDiffUtils()) {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_list,parent, false)
+        return ContactViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
+        val contact = getItem(position)
+        holder.bind(contact)
+    }
+
+    class ContactViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        private val tvName = view.findViewById<TextView>(R.id.tv_name)
+        private val tvTel = view.findViewById<TextView>(R.id.tv_tel)
+        private val imageView = view.findViewById<ImageView>(R.id.imageView)
+
+        fun bind(contact: Contact) {
+            tvName.text = contact.name
+            tvTel.text = contact.phone
+            imageView.setImageResource(contact.icon)
+
+        }
+
+    }
+
+    class ContactDiffUtils : DiffUtil.ItemCallback<Contact>(){
+        override fun areItemsTheSame(oldItem: Contact, newItem: Contact): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Contact, newItem: Contact): Boolean {
+           return oldItem.name == newItem.name
+        }
+
+    }
+
+}
--- app/src/main/java/com/devspace/recyclerview/MainActivity.kt
+++ app/src/main/java/com/devspace/recyclerview/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,69 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val rvlist = findViewById<RecyclerView>(R.id.rv_list)
+        val adapter = ContactListAdapter()
+
+        rvlist.adapter = adapter
+        rvlist.layoutManager = LinearLayoutManager(this)
+        adapter.submitList(contacts)
+
     }
 }
+
+val contacts = listOf(
+    Contact (name = "Cleiton",
+        phone = "(55) 79 955698655",
+        R.drawable.sample2
+    ),
+    Contact (name = "Cleidir",
+        phone = "(55) 79 955788655",
+        R.drawable.sample14
+    ),
+    Contact (name = "Isadora",
+        phone = "(55) 79 888698655",
+        R.drawable.sample11
+    ),
+    Contact (name = "Luciana",
+        phone = "(55) 79 955688622",
+        R.drawable.sample3
+    ),
+    Contact (name = "Chimbinha",
+        phone = "(55) 79 955697454",
+        R.drawable.sample12
+    ),
+    Contact (name = "Craque Neto",
+        phone = "(55) 79 955688555",
+        R.drawable.sample10
+    ),
+    Contact (name = "Carminha",
+        phone = "(55) 79 922688622",
+        R.drawable.sample7
+    ),
+    Contact (name = "Malu",
+        phone = "(55) 79 000488622",
+        R.drawable.sample4
+    ),
+    Contact (name = "Rita",
+        phone = "(55) 79 955688777",
+        R.drawable.sample15
+    ),
+    Contact (name = "Robert Niro",
+        phone = "(55) 79 955688440",
+        R.drawable.sample9
+    ),
+    Contact (name = "Lais",
+        phone = "(55) 79 024688622",
+        R.drawable.sample5
+    ),
+    Contact (name = "Clóvis",
+        phone = "(55) 79 955612122",
+        R.drawable.sample8
+    ),
+    Contact (name = "Marymar",
+        phone = "(55) 79 575688622",
+        R.drawable.sample6
+    ),
+
+)
